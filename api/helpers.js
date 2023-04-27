3d8b8a378d59b7904fe969ca90d1124b0b740476
Luke Cordova <lukethegreat000@gmail.com>
2023-04-27 00:46:50 -0700

    test

--- api/helpers.js
+++ api/helpers.js
@@ -2,6 +2,5 @@ export const constructNameMessage = (name = 'Luke Cordova') => {
   return {
     message: `My name is ${name}`,
     timestamp: new Date().getTime(),
-    test: `new Field`,
   };
 };
